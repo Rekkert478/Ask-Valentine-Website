048672bf98afb7c6763a1bd3340c53dba46800d3
Lukas Rieger <lukasrieger14@gmail.com>
2026-02-09 18:14:24 +0100

    added more features to the index page

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const noContainer = document.getElementById("no-container");
 const yesButton = document.getElementById("yes-btn");
 const noButton = document.getElementById("no-btn");
 
+let noClickCount = 0;
+let scale = 0.0;
+
 yesButton?.addEventListener("click", () => {
 	window.location.href = "yes.html";
 });
@@ -10,6 +13,16 @@ yesButton?.addEventListener("click", () => {
 noButton?.addEventListener("mouseover", () => {
     const min = 200;
     const max = 200;
+    noClickCount++;
+    scale += 0.5;
+
+    if(noClickCount > 3) {
+        yesButton.style.transform = `scale(${scale})`;
+    }
+
+    if(noClickCount >= 4) {
+        yesButton.classList.add("pulse");
+    }
 
     const distance = Math.random() * (max - min) + min;
     const angle = Math.random() * Math.PI * 2;
